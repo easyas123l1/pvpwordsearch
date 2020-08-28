4e16a582231976542dc34fd1928801e6d545d930
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-08-28 05:09:33 -0500

    tons of changes for socket.io

--- socket/socket.js
+++ socket/socket.js
@@ -7,6 +7,7 @@ const { v4: uuid } = require("uuid");
 let possibleWords = [];
 const rooms = {};
 const connections = {};
+const connectionsEmail = {};
 
 const loadPossibleWords = (file) => {
     try {
@@ -21,20 +22,61 @@ const loadPossibleWords = (file) => {
 loadPossibleWords("./socket/words.txt");
 
 /**
+ * Will update the state of a room and emit to all sockets
+ * @param room An object that represents a room from the `rooms` instance variable object
+ */
+const updateRoom = (room) => {
+    const responseObj = {
+        players: room.players,
+        id: room.id,
+        name: room.name,
+        state: room.state,
+    };
+
+    room.sockets.forEach((socket) => {
+        socket.emit("roomInfo", responseObj);
+    });
+};
+
+/**
  * Will connect a socket to a specified room
  * @param socket A connected socket.io socket
  * @param room An object that represents a room from the `rooms` instance variable object
  */
 const joinRoom = (socket, room) => {
-    room.sockets.push(socket);
-    socket.join(room.id, () => {
-        // store the room id in the socket for future use
-        socket.roomId = room.id;
-        console.log(socket.id, "Joined", room.id);
-        console.log(room.sockets.length);
-        // do some for loop on room.sockets.playerobj we need to figure out how that will work...
-        socket.emit("enteredRoom", room.id);
-    });
+    if (!socket.roomId) {
+        room.sockets.push(socket);
+        const playerObj = {
+            email: socket.email,
+            id: socket.id,
+        };
+        room.players.push(playerObj);
+        socket.join(room.id, () => {
+            // store the room id in the socket for future use
+            socket.roomId = room.id;
+            console.log(socket.id, "Joined", room.id);
+            console.log(room.sockets.length);
+
+            updateRoom(room);
+        });
+    }
+};
+
+/**
+ * Will make the socket leave the server
+ * @param socket A connected socket.io socket
+ */
+const disconnectServer = (socket) => {
+    leaveRooms(socket);
+    if (socket.email) {
+        delete connections[socket.id];
+        delete connectionsEmail[socket.email];
+        console.log("user disconnected  ", socket.email);
+        socket.email = undefined;
+        socket.id = undefined;
+        console.log(connections);
+        console.log(connectionsEmail);
+    }
 };
 
 /**
@@ -42,6 +84,7 @@ const joinRoom = (socket, room) => {
  * @param socket A connected socket.io socket
  */
 const leaveRooms = (socket) => {
+    socket.roomId = undefined;
     const roomsToDelete = [];
     for (const id in rooms) {
         const room = rooms[id];
@@ -50,24 +93,39 @@ const leaveRooms = (socket) => {
             socket.leave(id);
             // remove the socket from the room object
             room.sockets = room.sockets.filter((item) => item !== socket);
+            room.players = room.players.filter((item) => item.id !== socket.id);
+            // if there is other players we need to update the room
+            if (room.sockets.length > 0) {
+                // check host left lobby before game start.
+                if (room.hostId === socket.id && room.state === "FILLING") {
+                    // disconnect all sockets inform host left.
+                    room.sockets.forEach((item) => {
+                        item.emit("lobbyClosedHostLeft");
+                        leaveRooms(item);
+                    });
+                } else {
+                    updateRoom(room);
+                }
+            }
         }
         // Prepare to delete any rooms that are now empty
         if (room.sockets.length == 0) {
             roomsToDelete.push(room);
         }
     }
-
     // Delete all the empty rooms that we found earlier
     for (const room of roomsToDelete) {
         delete rooms[room.id];
     }
+
+    io.emit("rooms", getRooms());
 };
 
 const getRooms = () => {
     const roomNames = [];
     for (const id in rooms) {
-        const { name } = rooms[id];
-        const room = { name, id };
+        const { name, players, state } = rooms[id];
+        const room = { id, name, players, state };
         roomNames.push(room);
     }
     return roomNames;
@@ -85,48 +143,55 @@ io.on("connection", (socket) => {
             // if user hasn't been set yet.
             if (!socket.email) {
                 // check if user already logged in by email
-                if (email in connections) {
-                    socket.emit("alreadyConnected");
+                if (email in connectionsEmail) {
+                    // socket.emit("alreadyConnected"); //uncomment this line for production
+                    socket.id = uuid(); //comment out this line for production
+                    socket.email = email; //comment out this line for production
+                    connections[socket.id] = email; //comment out this line for production
+                    socket.emit("goodConnection", socket.id); //comment out this line for production
                 } else {
                     socket.id = uuid();
                     socket.email = email;
-                    connections[email] = socket.id;
-                    socket.emit("goodConnection");
+                    connections[socket.id] = email;
+                    connectionsEmail[email] = socket.id;
+                    socket.emit("goodConnection", socket.id);
                     console.log("a user connected  ", socket.email);
                     console.log(connections);
                 }
             } else if (socket.email === email) {
-                socket.emit("goodConnection");
+                socket.emit("goodConnection", socket.id);
             } else if (socket.email !== email) {
                 // !*TO BE DONE!*not the same user...
             }
-            const roomNames = getRooms();
-            socket.emit("rooms", roomNames);
+            socket.emit("rooms", getRooms());
         }
     });
     /**
      * Gets fired when a user wants to create a new room.
      */
     socket.on("createRoom", (roomName) => {
-        const room = {
-            id: uuid(), // generate a unique id for the new room, that way we don't need to deal with duplicates.
-            name: roomName,
-            sockets: [],
-            players: [],
-        };
-        rooms[room.id] = room;
-        // have the socket join the room they've just created.
-        joinRoom(socket, room);
-        const roomNames = getRooms();
-        io.emit("rooms", roomNames);
+        if (!socket.roomId) {
+            const room = {
+                id: uuid(), // generate a unique id for the new room, that way we don't need to deal with duplicates.
+                name: roomName,
+                sockets: [],
+                players: [],
+                // state will let us know what phase the room is in.
+                state: "FILLING",
+                hostId: socket.id,
+            };
+            rooms[room.id] = room;
+            // have the socket join the room they've just created.
+            joinRoom(socket, room);
+            io.emit("rooms", getRooms());
+        }
     });
 
     /**
      * Gets fired when user wants list of rooms.
      */
-    socket.on("getRoomNames", (data) => {
-        const roomNames = getRooms();
-        socket.emit("rooms", roomNames);
+    socket.on("getRoomNames", () => {
+        socket.emit("rooms", getRooms());
     });
 
     /**
@@ -137,39 +202,34 @@ io.on("connection", (socket) => {
         joinRoom(socket, room);
     });
 
+    /* 
+    what i have made up my mind is game settings will need to be set 
+    before the game room is created.  From there the
+    creator should be assumed host and can start lobby whenever they want.
+    If the creator leaves before the game starts the whole lobby 
+    should be notified and backed out.  If the game has already started 
+    nothing will happen.
+    */
+
     /**
      * Gets fired when a player leaves a room.
      */
     socket.on("leaveRoom", () => {
         leaveRooms(socket);
-        const roomNames = getRooms();
-        socket.emit("rooms", roomNames);
     });
 
     /**
      * Gets fired when a player logs out of google account
      */
     socket.on("disconnecting", () => {
-        if (socket.email) {
-            console.log("user disconnected  ", socket.email);
-            delete connections[socket.email];
-            console.log(connections);
-            socket.email = undefined;
-            socket.id = undefined;
-        }
-        leaveRooms(socket);
+        disconnectServer(socket);
     });
 
     /**
      * Gets fired when a player disconnects from the server.
      */
     socket.on("disconnect", () => {
-        if (socket.email) {
-            console.log("user disconnected  ", socket.email);
-            delete connections[socket.email];
-            console.log(connections);
-        }
-        leaveRooms(socket);
+        disconnectServer(socket);
     });
 });
 
