81510a78315c39715f831e33594dda362c165403
Andrew Wilson <andrew.wilson90@hotmail.com>
2020-12-05 15:52:15 -0600

    update testing db directory

--- knexfile.js
+++ knexfile.js
@@ -1,42 +1,42 @@
 module.exports = {
-  development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/jswordsearch.db3",
+    development: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./data/jswordsearch.db3",
+        },
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            },
+        },
     },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
-  },
 
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./data/migrations",
+    production: {
+        client: "pg",
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            tableName: "knex_migrations",
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
     },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
 
-  testing: {
-    client: "sqlite3",
-    connection: { filename: "./data/jswordsearch.db3" },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
+    testing: {
+        client: "sqlite3",
+        connection: { filename: "./data/testjswordsearch.db3" },
+        useNullAsDefault: true,
+        migrations: {
+            directory: "./data/migrations",
+        },
+        seeds: { directory: "./data/seeds" },
     },
-    seeds: { directory: "./data/seeds" },
-  },
 };
